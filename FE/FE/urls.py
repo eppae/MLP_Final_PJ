5fdd5432a0240f413f295d7c5d1df4a115c431e8
Imshyeon <kzoen0040@naver.com>
2023-11-10 15:39:04 +0900

    re..

--- FE/FE/urls.py
+++ FE/FE/urls.py
@@ -4,6 +4,7 @@ from .views import (
     community,
     contact,
     login,
+    forgot_id,
     forgot_pw,
     signup,
     post_form,
@@ -14,7 +15,10 @@ from .views import (
     user_profile,
     admin_profile,
     contact_list,
-    contact_detail
+    contact_detail,
+    
+    forgot_id_result,
+    reset_pw
 )
 from django.contrib import admin
 from django.urls import path
@@ -25,7 +29,8 @@ urlpatterns = [
     path("community", community, name="community"),
     path("contact", contact, name="contact"),
     path("login", login, name="login"),
-    path("forgot_pw", forgot_pw, name="forgot-password"),
+    path("forgot_id", forgot_id, name="forgot-id"),
+    path("forgot_pw", forgot_pw, name="forgot_pw"),
     path("signup", signup, name="signup"),
     path("post_form", post_form, name="post_form"),
     path("support", support, name="support"),
@@ -36,4 +41,8 @@ urlpatterns = [
     path("admin_profile", admin_profile, name="admin_profile"),
     path("contact_list", contact_list, name="contact_list"),
     path("contact_detail", contact_detail, name="contact_detail"),
+    
+    # ========================== 아래는 페이지 확인 차 만든 것임. 삭제해도 됨! ===============================
+    path("id_res", forgot_id_result, name="forgot_id_result"),
+    path("reset_pw", reset_pw, name="reset_pw"),
 ]
